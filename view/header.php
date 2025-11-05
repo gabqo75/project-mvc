57ace3013435ac0a3010b790d0bdce4e90fcc420
Trickzy22 <thomas.pires@efrei.net>
2025-11-05 15:25:48 +0100

    view 1

---
+++ view/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smartbike</title>
+</head>
+<body>
+<header>
+<img src="../view/logo.png" alt="logo">
+    <h1>Smartbike</h1>
+    <nav>
+        <a href="index.php">Accueil</a>
+        <a href="?page=produits">Vélos</a>
+        <a href="?page=contact">Contact</a>
+    </nav>
+</header>
